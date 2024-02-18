569005e2c8c92f6362ae56b19db41e006147d130
said-oussama <61437831+said-oussama@users.noreply.github.com>
2024-02-18 21:59:06 +0100

    commit3

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6690a2e177f2623306f0592cb63aa9bf4de8d87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87dd77666ee6cd6e6be1ea6c117dcca9c9f03d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
